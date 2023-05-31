d7da9ee8a236f5f32f9069274da6e58f36ac55cb
Foo Mun Leong <foo-mun.leong@keysight.com>
2023-05-31 07:36:18 +0000

    My first change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello there ! I am simple Java app");
+        System.out.println("FM was here");
     }
 
     public String getMessage() {
